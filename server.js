6459ddce23d0f04252a1d80ae014886a461c423f
Gus <gustavo.gerolmini@gmail.com>
2025-02-14 22:01:33 -0500

    Use a Template Engine's Powers

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 4. Render the index page using res.render
 app.route('/').get((req, res) => {
-
-});
+  res.render('index/pug');
+}); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
